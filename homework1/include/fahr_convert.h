ccd7c15851179e80afd1f49598cf509bd545b20e
Brandon Tarney <btarney1@dev4.apl.jhu.edu>
2017-02-15 00:18:33 -0500

    got my header files back

---
+++ homework1/include/fahr_convert.h
@@ -0,0 +1,13 @@
+/*
+   * Library to convert celsius to fahrenheit
+   *
+   * @author brandon tarney
+   * @date  2/17/2017
+   */
+
+#ifndef getarg_h
+#define getarg_h
+
+double convert_to_fahr(double celsius);
+
+#endif
